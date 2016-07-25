689efff38852e503d8b88649c316959a86671ffb
Pawan Mishra <pawan@clay.fish>
2016-07-25 23:04:18 +0530

    Better documentation and output

--- findGreater.c
+++ findGreater.c
@@ -7,18 +7,31 @@
 
 int max(int, int);
 
+/**
+ * Max among 3 numbers
+ * @param a first number
+ * @param b second number
+ * @param c third number
+ * @return max among the three numbers
+ */
 int maxIn3(int, int, int);
 
 int main() {
     int x, y, z;
-    printf("Please inter three numbers");
+    printf("Please enter three numbers: ");
     scanf("%d%d%d", &x, &y, &z);
-//    printf("Max is %d", max(x, y));
-    printf("max is %d", maxIn3(x, y, z));
+    printf("Max between %d and %d is %d.\n", x, y, max(x, y));
+    printf("Max among %d, %d and %d is %d.", x, y, z, maxIn3(x, y, z));
     getch();
     return 0;
 }
 
+/**
+ *
+ * @param a an int
+ * @param b an int
+ * @return max between a and b
+ */
 int max(int a, int b) {
     return a > b ? a : b;
 }
