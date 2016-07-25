f3c4fff32ab238bc90a07200a63cfef1badf2fc2
Adarsh Shukla <adarsh@clay.fish>
2016-07-25 18:45:52 +0530

    Add even odd.c

--- EvenOdd.c
+++ EvenOdd.c
@@ -1,4 +1,23 @@
 //
 // Created by akela on 25-07-2016.
 //
+#include <stdio.h>
+#include <conio.h>
 
+int main() {
+    int i;
+    printf("Please inter a number");
+    scanf("%d", &i);
+
+    if (i < 1 || i > 100) {
+        printf("Sorry Please insert a number between 1-100");
+    } else {
+        if (i % 2 == 0) {
+            printf("This is an even number");
+        } else {
+            printf("this is an odd number");
+        }
+    }
+    getch();
+    return 0;
+}
