73f950eb1e40dce554a277d1879fbde706bc177e
Pawan Mishra <pawan@clay.fish>
2016-07-26 17:08:46 +0530

    Get the Table and Sum

---
+++ printTable.c
@@ -0,0 +1,20 @@
+//
+// Created by Pawan Mishra on 26-07-2016.
+//
+
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    int x, y, z=0, t;
+    printf("Enter a number: ");
+    scanf("%d" , &x);
+    for (y = 1; y <=10; y++) {
+        t= x*y;
+        printf("%d*%d=%d \n", x, y, t);
+        z= z+t;}
+    printf("Total of=%d",z);
+
+
+    getch();
+    return 0;
+}
