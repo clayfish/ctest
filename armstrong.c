adb7797ad4165f9f52ac32016d8fc7d3362951e4
Adarsh Shukla <adarsh@clay.fish>
2016-07-27 17:36:46 +0530

    Check no Armstorng or not

---
+++ armstrong.c
@@ -0,0 +1,28 @@
+//
+// Created by akela on 27-07-2016.
+//
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int i, sum = 0, rem, z;
+    printf("Please enter a number: ");
+    scanf("%d", &i);
+    z=i;
+    while (i != 0) {
+        rem = i % 10;
+        sum = sum + rem * rem * rem;
+        i = i / 10;
+    }
+    printf("sum= %d\n", sum);
+
+    if (sum==z){
+        printf("Your number is an armstrong number");
+    }
+    else {
+        printf("Your number is not an armstrong number");
+    }
+    getch();
+    return 0;
+}
