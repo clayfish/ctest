7faba72236c0dbf72f812867bb83e9b03d509bf6
Adarsh Shukla <adarsh@clay.fish>
2016-07-28 16:25:14 +0530

    Merge branch 'master' of C:\Users\akela\Documents\workspace\ctest with conflicts.

---
+++ sumofdigits1.c
@@ -0,0 +1,26 @@
+//
+// Created by akela on 28-07-2016.
+//
+
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int i, rem, x, y, z;
+    printf("Enter an value that have max 3 digits");
+    scanf("%d", &i );
+    while(i!=o){
+        rem= i%10;
+        x= rem;
+        y= rem;
+        z= rem;
+        i=i/10;
+        printf("%d", x);
+        printf("%d", y);
+        printf("%d", z);
+    }
+
+    getch();
+    return 0;
+}
