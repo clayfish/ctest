e0a4121c6d602504e25e76f84d9abced050fe534
Adarsh Shukla <adarsh@clay.fish>
2016-07-29 00:03:58 +0530

    printing addresses of variables

---
+++ Leapyear.c
@@ -0,0 +1,22 @@
+//
+// Created by akela on 28-07-2016.
+//
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int i, n = 4, z, y, k = 400;
+    printf("Enter a year: ");
+    scanf("%d", &i);
+    z = i % n;
+    y = i % k;
+    if (z == 0 && y != 0) {
+        printf("This is a leap year");
+    }
+    else {
+        printf("This is not a leap year");
+    }
+    getch();
+    return 0;
+}
