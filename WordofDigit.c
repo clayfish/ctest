5004cebc30ac70a4f6d9907931439f2981f9a2ab
Pawan Mishra <pawan@clay.fish>
2016-07-30 12:10:45 +0530

    prime number and word of digit programs

---
+++ WordofDigit.c
@@ -0,0 +1,57 @@
+//
+// Created by Pawan Mishra on 29-07-2016.
+//
+
+
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    char z;
+    printf("Enter your digit:");
+    scanf("%c" , &z);
+    switch(z){
+        case '0':
+            printf("this is zero");
+            break;
+
+        case '1':
+            printf("this is One");
+            break;
+
+        case '2':
+            printf("this is Two");
+            break;
+
+        case '3':
+            printf("this is Three");
+            break;
+
+        case '4':
+            printf("this is Four");
+            break;
+
+        case '5':
+            printf("this is Five");
+            break;
+
+        case '6':
+            printf("this is Six");
+            break;
+
+        case '7':
+            printf("this is Seven");
+            break;
+
+        case '8':
+            printf("this is Eight");
+            break;
+
+        case '9':
+            printf("this is Nine");
+            break;
+        default:
+            printf("This is not in 0 to 9 Range");
+
+    }
+    getch();
+}
